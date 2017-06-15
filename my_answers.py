b759097ebed00932fcce34905348dff3ce42472e
Brian Cooley <coolmodev@gmail.com>
2017-06-14 20:09:17 -0500

    Add window_transform_series to my_answers

--- my_answers.py
+++ my_answers.py
@@ -6,19 +6,23 @@ from keras.layers import LSTM
 import keras
 
 
-# TODO: fill out the function below that transforms the input series 
+# TODO: fill out the function below that transforms the input series
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series,window_size):
     # containers for input/output pairs
     X = []
     y = []
 
-    # reshape each 
+    for i in range(window_size, len(series)):
+        X.append(series[i-window_size : i])
+        y.append(series[i])
+
+    # reshape each
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
     y.shape = (len(y),1)
-    
+
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
@@ -31,7 +35,7 @@ def clean_text(text):
     # find all unique characters in the text
 
 
-    # remove as many non-english characters and character sequences as you can 
+    # remove as many non-english characters and character sequences as you can
 
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -39,7 +43,7 @@ def window_transform_text(text,window_size,step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
-    
 
-    
+
+
     return inputs,outputs
