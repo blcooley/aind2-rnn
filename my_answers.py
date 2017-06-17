8df542c115b5b1c07c824bab3b6634eca166f235
Brian Cooley <coolmodev@gmail.com>
2017-06-17 09:49:25 -0500

    Add code to my_answers.py

--- my_answers.py
+++ my_answers.py
@@ -27,16 +27,27 @@ def window_transform_series(series,window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(step_size, window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size,1)))
+    model.add(Dense(1))
+    return model
 
 
 ### TODO: list all unique characters in the text and remove any non-english ones
 def clean_text(text):
     # find all unique characters in the text
+    chars = set(text)
 
+    chars_to_keep = set([' ', '!', ',', '.', ':', ';', '?', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                         'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                         'v', 'w', 'x', 'y', 'z'])
+    chars_to_remove = chars - chars_to_keep
 
     # remove as many non-english characters and character sequences as you can
+    for char in chars_to_remove:
+        text = text.replace(char, ' ')
 
+    return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text,window_size,step_size):
@@ -44,6 +55,8 @@ def window_transform_text(text,window_size,step_size):
     inputs = []
     outputs = []
 
-
+    for i in range(window_size, len(text), step_size):
+        inputs.append(text[i-window_size:i])
+        outputs.append(text[i])
 
     return inputs,outputs
